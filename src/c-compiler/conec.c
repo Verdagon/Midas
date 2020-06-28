f85f6edd622d8f1b597b67e0b3150604d57dbd99
Kevin Navero <knavero@gmail.com>
2020-06-28 00:16:45 -0700

    src: cleanup
    removed excess src/c-compiler/genllvm files

--- src/c-compiler/conec.c
+++ src/c-compiler/conec.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
     // Parse source file, do semantic analysis, and generate code
     ModuleNode *modnode = NULL;
     if (!errors)
-        genmod(&gen, modnode);
+        genMod(&gen, modnode);
 
     genClose(&gen);
     errorSummary();
